b0b0a805346806e61bbdda54d0c0b843b3b6a236
tejaswini <tejaswiniprathapaneni@gmail.com>
2026-03-23 14:04:37 +0530

    Initial commit

--- studentapp/admin.py
+++ studentapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Student  
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('student_name', 'age', 'email_id', 'date_of_birth', 'enrolled_in')
